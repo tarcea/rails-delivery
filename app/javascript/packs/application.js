dd5aba6df43cc8c7827e26a6198b61e6d2526a8e
Gheorghe Tarcea <tarcea@gmail.com>
2020-05-12 15:04:33 +0200

    trials

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,8 +1,8 @@
 import "bootstrap";
 
-const element = document.querySelector('.hover-menu');
-element.addEventListener("click", event => {
-  location.reload();
-});
+// const element = document.querySelector('.hover-menu');
+// element.addEventListener("click", event => {
 
+// });
 
+document.addEventListener("touchstart", function(){}, true);
