1fdcf3e23c8252a9a55335f3286bdba59a8f6f15
Gheorghe Tarcea <tarcea@gmail.com>
2020-06-01 12:34:57 +0200

    js text animation - trials

--- app/assets/javascripts/test.js
+++ app/assets/javascripts/test.js
@@ -1,13 +1,16 @@
-// let a = 0;
-let arr = ["you", "grandma", "everibody"];
+let a = 0;
+let arr = ["you", "grandma", "pets", "aliens", "kids", "pigs", "elephants", "cats", "dogs", "flowers", "trees", "monkies", "cars", "and much more..."]
 let cont = document.getElementById('effect');
-
 const animate = () => {
-//   if (a < textEffect.length) {
-    // cont.innerHTML = textEffect.charAt(a);
-    cont.innerHTML = arr[Math.floor(Math.random() * arr.length)];
-    // a ++
-    setTimeout(animate, 800);
-  // }
+
+    if (a < arr.length) {
+      // cont.innerHTML = textEffect.charAt(a);
+      // cont.innerHTML = arr[Math.floor(Math.random() * arr.length)];
+      cont.innerHTML = arr[a];
+      a ++
+      setTimeout(animate, 2000);
+    }
+
 }
+
 animate();
