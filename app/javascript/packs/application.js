56a7b317e98b1e09f5092917d29187fa04fd8d76
Gheorghe Tarcea <tarcea@gmail.com>
2020-06-28 16:30:41 +0200

    text animation infinite loop, typewriter npm action

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,8 +6,17 @@ import "bootstrap";
 // });
 // document.addEventListener("touchstart", function(){}, true);
 // console.log("Hello from app/javascript/packs/application.js!");
-import { animate } from '../animate';
-animate();
+// import { animate } from '../animate';
+// animate();
 // setInterval(() => {
 //   animate();
 // }, 1000);
+import Typewriter from 'typewriter-effect/dist/core';
+
+new Typewriter('#effect', {
+  strings: ["you", "grandma", "pets", "aliens", "kids", "pigs",
+            "elephants", "cats", "dogs", "flowers", "trees", "monkies",
+            "cars", "cows", "wine", "beer"],
+  autoStart: true,
+  loop: true,
+});
---
+++ app/javascript/typewriter.js
@@ -0,0 +1,10 @@
+import Typewriter from 'typewriter-effect/dist/core';
+
+// const cont = document.getElementById('effect');
+
+new Typewriter('#effect', {
+  strings: ['Hello', 'World'],
+  autoStart: true,
+  loop: true,
+});
+
