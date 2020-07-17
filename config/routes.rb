997281d37702f7c9f5f70cb910c65e3644ce859f
Gheorghe Tarcea <tarcea@gmail.com>
2020-07-17 15:46:24 +0200

    user login

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
+  # devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :projects
