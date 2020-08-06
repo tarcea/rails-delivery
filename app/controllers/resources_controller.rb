85d3bb5d6ae0eb5df12c613be89469dfc90fcc31
Gheorghe Tarcea <tarcea@gmail.com>
2020-08-06 15:55:46 +0200

    resources scrapping

--- app/controllers/resources_controller.rb
+++ app/controllers/resources_controller.rb
@@ -11,4 +11,19 @@ class ResourcesController < ApplicationController
     @resource = Resource.find(params[:id])
     # raise
   end
+
+  def create
+    @resource = Resource.new(resource_params)
+    if @resource.save
+      redirect_to @resource, notice: 'created OK'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def resource_params
+    params.require(:resource).permit(:url, :description, :discipline)
+  end
 end
