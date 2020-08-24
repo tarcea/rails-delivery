9dbf5b6d3ad203e5735d529a0abf26b0c875d5f3
Gheorghe Tarcea <tarcea@gmail.com>
2020-08-24 21:33:58 +0200

    dinamically curent year in the footer

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -36,3 +36,5 @@ new Typewriter('#effect', {
 });
 
 
+
+
--- app/javascript/packs/hello_react.jsx
+++ app/javascript/packs/hello_react.jsx
@@ -2,12 +2,13 @@
 // like app/views/layouts/application.html.erb. All it does is render <div>Hello React</div> at the bottom
 // of the page.
 
-import React from 'react'
-import ReactDOM from 'react-dom'
-import PropTypes from 'prop-types'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import PropTypes from 'prop-types';
+import { CURRENT_YEAR } from './config';
 
 const Hello = props => (
-  <small><div className="text-center">&copy; Copyright 2020 {props.name} </div></small>
+  <small><div className="text-center">&copy; Copyright {CURRENT_YEAR} {props.name} </div></small>
 )
 
 Hello.defaultProps = {
